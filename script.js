10bbd9743943c866b505d17184e660769c25b992
victor kyalo <victor.kyalo@student.moringaschool.com>
2024-01-11 18:06:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1995 },
+    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Mairie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+];
+
+const people = [
+    'Beck , Glenn','Becker , Carl','Benn , Tony','Baker , Jennifer','Brown , David', 'Bell , Sarah','Barnes , Michael','Bennett , Emily','Blake , Robert','Brewer , Maria',
+    'Bryant , James','Burke , Jessica','Bowen , Christopher','Bates , Amanda',
+    'Bowers , Daniel','Bennett , Melissa','Bush , Brian','Black , Ashley',
+    'Baldwin , Matthew','Briggs , Olivia','Barnett , William','Byrd , Megan','Burton , Richard',
+    'Bass , Elizabeth', 'Baldwin , Joseph','Boyd , Kimberly','Beasley , Patrick',
+    'Barrera , Laura','Buckley , Tyler','Bailey , Michelle','Barton , Thomas','Bullock , Christina','Blake , Daniel','Burgess , Heather','Bishop , Kevin',
+    'Bullock , Stephanie','Bush , Charles','Brady , Rebecca','Benson , Andrea', 'Bass , Timothy',
+    'Becker , Jessica','Black , Benjamin', 'Bruce , Laura','Bauer , Nicholas','Barnett , Brandon','Bishop , Victoria','Brady , Jeffrey',
+    'Beard , Natalie', 'Bolton , Brandon', 'Bowers , Jennifer','Burns , Christopher','Bentley , Amanda','Bass , Rachel',
+    'Boone , Nicholas','Baldwin , Hannah','Barker , Zachary','Brewer , Victoria', 'Bates , Marie', 'Burke , Eric',
+    'Blackwell , Allison','Brock , Jason','Beasley , Rachel','Briggs , Sean',
+    'Barnes , Phillip','Blair , Brittany','Bush , Gerald',
+    'Browning , Justin', 'Bruce , Lauren','Bolton , Marcus','Barnes , Courtney','Barron , Derek',
+    'Buckley , Gregory','Bates , Christine','Brewer , Russell', 'Baker , Jacqueline',
+    'Baldwin , Justin','Benson , Derek','Browning , Patrick','Barron , Marie',
+    'Barnes , Brittany','Brennan , Jeremy', 'Bradshaw , Lindsay','Becker , Jordan',
+    'Burns , Julia','Bass , Ronald','Briggs , Cynthia','Barnes , Gary','Baldwin , Dana','Barnett , Gerald','Bennett , Gerald',
+  ];
+  
+//   Array.prototype.filter()
+//  1. filter the list of inventors for those who were born in i500's
+ const fifteen = inventors.filter(function(inventor){
+    if (inventor.year >= 1500 && inventor.year < 1600){
+        return true;//keep it 
+    }
+ })
+ console.table(fifteen);
+   
+// Array.prototype.map()
+// 2.Give us an array of the inventory first and last names
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.log(fullName); 
+
+// Array.prototype.sort()
+// 3.sort the inventors by birthdate, oldest to youngest
+
+// Array.prototype.reduce()
+// 4.How many years did all the inventors live?
+
+// 5.Sort the investors by years lived 
+
+// create a first of Boulevards in paris that contain 'de' anywhere in the name
+// 6.https://en.wikipidea.org/wiki/category:Boulevards_in_paris
+
+// 7.sort Exercise
+// sort the people alphabetically by last name
+
+// 8.reduce Exercise
